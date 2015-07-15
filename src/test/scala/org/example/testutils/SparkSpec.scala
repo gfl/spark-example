1aee99f188e73e51d2e7d3512bd1d4842f88582c
Gracia Fernandez <graciafdez@gmail.com>
2015-07-15 14:51:17 +0100

    Added before and after test suite to start/stop Spark context

---
+++ src/test/scala/org/example/testutils/SparkSpec.scala
@@ -0,0 +1,38 @@
+package org.example.testutils
+
+import org.apache.spark._
+import org.scalatest._
+
+trait SparkSpec extends BeforeAndAfterEach {
+  this: Suite =>
+
+  private val master = "local[2]"
+  private val appName = this.getClass.getSimpleName
+
+  private var _sc: SparkContext = _
+
+  def sc = _sc
+
+  val conf: SparkConf = new SparkConf()
+    .setMaster(master)
+    .setAppName(appName)
+
+  override def beforeEach(): Unit = {
+    super.beforeEach()
+
+    // To avoid Akka rebinding to the same port, since it doesn't unbind immediately on Shutdown
+    _sc = new SparkContext(conf)
+  }
+
+  override def afterEach(): Unit = {
+    if (_sc != null) {
+      _sc.stop()
+      _sc = null
+    }
+    // To avoid Akka rebinding to the same port, since it doesn't unbind immediately on shutdown
+    System.clearProperty("spark.master.port")
+
+    super.afterEach()
+  }
+
+}
